f0b7958f1f29eeef9539d27645c60a14ff7a683f
LORDPQK <1425143372@qq.com>
2020-09-19 17:13:08 +0800

    提交作业1.1

---
+++ src/homework/java1_1.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class java1_1 {
+    public static void main(String[] args) {
+        Circle o = new Circle();
+        Square z = new Square();
+     }
+}
--- src/java1/firat.java
+++
@@ -1,7 +0,0 @@
-package java1;
-
-public class firat {
-    public static void main(String[] args) {
-        System.out.println("Hollow world!");
-    }
-}
