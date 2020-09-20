94d2db6e2c660a7a64c838f72ea89b5eeb6920de
LORDPQK <1425143372@qq.com>
2020-09-20 17:42:58 +0800

    习题1.3

---
+++ src/homework/java1_3.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class java1_3 {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        int a = -70 ;
+        circle.moveHorizontal(a);
+
+
+    }
+}
